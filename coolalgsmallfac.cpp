8f6890c5badc752cf0842d0173c7fc2e687610c0
Stierliz <alshtirbul@gmail.com>
2017-12-17 16:51:53 +0200

    Adding commentaries

--- coolalgsmallfac.cpp
+++ coolalgsmallfac.cpp
@@ -3,18 +3,20 @@ using namespace std;
 int main() {
 int t;
     cin>>t;
-    while(t--) {
+    while(t--) {   
         int a[200],rem,i=0,n,m=0,flag;
         cin>>n;
         flag = n; 
  
+	//putting the number into the array
         while(flag!=0)  {
             rem = flag%10;
             a[m] = rem;
             flag = flag/10; 
              m++;
         } 
- 
+ 	
+	//putting the factorial of this number into the array backwards
          int temp,x=0,index=0;
          for(i=2;i<n;i++)  {
              temp = 0;
@@ -24,6 +26,7 @@ int t;
                  temp = x/10;
               }
   
+	     //putting the temp number in the array backwards
               while(temp!=0) {
                   a[index] = temp % 10;
                   temp = temp/10;
